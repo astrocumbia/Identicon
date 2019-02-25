ed2ff8dabae08a391488c88d3828df826125751e
Irvin Castellanos <irvin.py@gmail.com>
2019-02-24 23:20:09 -0600

    Add image.ex

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,5 +3,20 @@ defmodule Identicon do
   Documentation for Identicon.
   """
 
-  
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
 end
